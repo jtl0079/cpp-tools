f469c69a2878f36d18c01ae58fe9ed60cc837c75
name_lss01 <you@example.com>
2025-11-17 16:00:11 +0800

    first save with few of file

---
+++ TOpenMP.cpp
@@ -0,0 +1,13 @@
+#include "TOpenMP.h"
+
+
+#include <iostream>
+
+
+namespace qwe {
+	void printu() {
+		using namespace std;
+		cout << "hie";
+	}
+
+}
---
+++ TOpenMP.h
@@ -0,0 +1,12 @@
+#pragma once
+
+
+
+namespace qwe {
+	void printu();
+
+}
+class TOpenMP
+{
+};
+
